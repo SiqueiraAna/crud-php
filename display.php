fc054f8c79b72bd30896db90ac4979ecf9bbcb6a
Ana <keel_siqueira@hotmail.com>
2021-09-10 17:20:42 -0300

    Changes

--- display.php
+++ display.php
@@ -8,7 +8,7 @@ include 'config/db.php';
     <meta charset=UTF-8>
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <Title>Página Inicial</Title>
+    <Title>Display</Title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
--- update.php
+++ update.php
@@ -1,6 +1,11 @@
 <?php
 include 'config/db.php';
 $id_pessoa=$_GET['updateid_pessoa'];
+
+// $sql="SELECT * FROM `pessoas` where id_pessoa=$id_pessoa";
+// $result=query($con,$sql);
+// $rows = $statement->fetchall(PDO::FETCH_ASSOC);
+
 if(isset($_POST['submit'])){
     $nome=$_POST['nome'];
     $endereco=$_POST['endereco'];
@@ -8,15 +13,13 @@ if(isset($_POST['submit'])){
     $email=$_POST['email'];
     $data_nascimento=$_POST['data_nascimento'];
 
-    // $statement =$con->query("SELECT * FROM pessoas");
-    // $rows = $statement->fetchall(PDO::FETCH_ASSOC);
 
     $sql = $con->prepare ("update`pessoas` set id_pessoa='$id_pessoa', nome='$nome',endereco='$endereco', 
-    telefone='$telefone', email='$email', data_nascimento='$data_nascimento'");
+    telefone='$telefone', email='$email', data_nascimento='$data_nascimento' where id_pessoa=$id_pessoa");
     $result=query($con,$sql);
 
     if($result){
-      // echo "update com sucesso";
+      echo "update com sucesso";
      header('location:display.php');
        
     }else{
@@ -26,16 +29,6 @@ if(isset($_POST['submit'])){
    
 }
 
-    // $sql="update `pessoas` set id_pessoa = 'id_pessoa', nome = 'nome', endereco = 'endereco', 
-    // telefone = 'telefone', email = 'email', data_nascimento = 'data_nascimento' where id_pessoa = 'id_pessoa'";
-    // $result=query($con,$sql);
-    // if($result){
-    //   echo "update sucesso!";
-          
-    // // header('location:display.php');
-    // }else{
-
-    // }
 ?>
 <!doctype html>
 <html lang="en">
@@ -47,7 +40,7 @@ if(isset($_POST['submit'])){
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
-    <title>Update Cadastro</title><br>
+    <title>Update</title><br>
  
     <script>
     function funcao1()
@@ -59,7 +52,6 @@ if(isset($_POST['submit'])){
   </head>
   <body>
   <div style="text-align:center; font-size: 2.1em; color: #000000;">Atualize seu cadastro!</div>
-  
     <div class="container">
     <form method="post">
     <div class="form-group">
@@ -87,7 +79,7 @@ if(isset($_POST['submit'])){
     <input type="date" class="form-control" placeholder="Data Nascimento" name="data_nascimento" autocomplete="off" value=<?php echo $data_nascimento; ?>>
     </div>
     <br>
-  <button type="submit" name="submit" class="btn btn-success" onclick="funcao1()">Update</button>
+  <button type="submit" name="submit" class="btn btn-outline-success" onclick="funcao1()">Update</button>
 </form>
     </div>
    
